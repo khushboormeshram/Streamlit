2b7019738e622a4fcb888de15fad2233fdd5e903
khushboormeshram <165081307+khushboormeshram@users.noreply.github.com>
2025-02-23 03:19:28 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+# import streamlit as st
+# st.write('Welcome to Streamlit!')
+# if st.button('Good Morning'):
+#     st.write("Hello there, very warm good morning to you!")
+# else:
+#     st.write("Good morning button not clicked")
+
+import numpy as np
+import pandas as pd
+import altair as alt
+import streamlit as st
+
+st.write("hello its me practicing streamlit")
+st.write(123)
+df=pd.DataFrame({'a1':[4,23,4], 'b':[8,0,3]})
+st.write(df)
+st.write('Hello, *World!* :sunglasses:')
